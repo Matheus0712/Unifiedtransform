7d4468caddab480a1a1aea2da82ff77188805a76
Matheus0712 <59248658+Matheus0712@users.noreply.github.com>
2024-07-09 13:25:54 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp .env.example .env
 docker-compose up -d
-echo -e "mysql -u root -p"your_mysql_root_password"\nSHOW DATABASES;\n GRANT ALL ON unifiedtransform.* TO 'unifiedtransform'@'%' IDENTIFIED BY 'secret'; \nFLUSH PRIVILEGES;" > comandos.sql
-cat comandos.sql | docker exec -i db sh
+echo -e "SHOW DATABASES;\n GRANT ALL ON unifiedtransform.* TO 'unifiedtransform'@'%' IDENTIFIED BY 'secret'; \nFLUSH PRIVILEGES;" > comandos.sql
+cat comandos.sql | docker exec -i db mysql -u root -p"your_mysql_root_password"
 echo -e "composer install\nphp artisan key:generate\nphp artisan config:cache\nphp artisan migrate:fresh --seed" > composer
 cat composer | docker exec -i app sh
